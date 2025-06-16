8dd9d8af92b993115fdc40816e4c67032be6c377
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-16 08:48:15 +0200

    Set up app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes/index")
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded ({ extended: true }));
+app.use("/". indexRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
