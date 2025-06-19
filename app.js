1bca083701bdc1eb9a079d3c0af6136cfda48a72
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-19 10:56:08 +0200

    Update index.ejs and app.js for db use

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const indexRouter = require("./routes/index")
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded ({ extended: true }));
 app.use("/", indexRouter);
 
